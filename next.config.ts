9f3a5ce4d604c0dbed0d8e488d715d8290db3ef3
abdullahnadim <meernadim@gmail.com>
2026-04-23 21:01:09 +0600

    Fix : error

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,11 @@ const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development", 
   register: true,
-  skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // <-- THIS SILENCES THE ERROR
+  turbopack: {}, 
 };
 
 export default withPWA(nextConfig);
